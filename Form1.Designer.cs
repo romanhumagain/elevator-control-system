454a3e0118d9e164d5a4e7cb46a836044b67705c
romanhumagain <romanhumagain@gmail.com>
2024-10-22 19:06:47 +0545

    added functionality for opening and closing door

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -39,21 +39,34 @@
             this.closeDoorBtn = new System.Windows.Forms.Button();
             this.liftTimer = new System.Windows.Forms.Timer(this.components);
             this.panel1 = new System.Windows.Forms.Panel();
-            this.logsLbl = new System.Windows.Forms.Label();
             this.logsGridView = new System.Windows.Forms.DataGridView();
+            this.logsLbl = new System.Windows.Forms.Label();
+            this.groundFloorDoorPanelL = new System.Windows.Forms.PictureBox();
+            this.groundFloorDoorPanelR = new System.Windows.Forms.PictureBox();
+            this.doorTimer = new System.Windows.Forms.Timer(this.components);
+            this.firstFloorDoorPanelL = new System.Windows.Forms.PictureBox();
+            this.firstFloorDoorPanelR = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.elevator)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.displayBoard)).BeginInit();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.logsGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.groundFloorDoorPanelL)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.groundFloorDoorPanelR)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.firstFloorDoorPanelL)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.firstFloorDoorPanelR)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.SuspendLayout();
             // 
             // elevator
             // 
             this.elevator.Image = ((System.Drawing.Image)(resources.GetObject("elevator.Image")));
-            this.elevator.Location = new System.Drawing.Point(91, 377);
+            this.elevator.Location = new System.Drawing.Point(118, 437);
             this.elevator.Name = "elevator";
-            this.elevator.Size = new System.Drawing.Size(151, 254);
+            this.elevator.Size = new System.Drawing.Size(168, 260);
             this.elevator.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.elevator.TabIndex = 0;
             this.elevator.TabStop = false;
@@ -62,9 +75,9 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(315, 1);
+            this.pictureBox1.Location = new System.Drawing.Point(462, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(195, 630);
+            this.pictureBox1.Size = new System.Drawing.Size(195, 695);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
@@ -72,7 +85,7 @@
             // displayBoard
             // 
             this.displayBoard.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.displayBoard.Location = new System.Drawing.Point(361, 81);
+            this.displayBoard.Location = new System.Drawing.Point(511, 193);
             this.displayBoard.Name = "displayBoard";
             this.displayBoard.Size = new System.Drawing.Size(102, 129);
             this.displayBoard.TabIndex = 2;
@@ -83,7 +96,7 @@
             // 
             this.firstFloorBtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("firstFloorBtn.BackgroundImage")));
             this.firstFloorBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.firstFloorBtn.Location = new System.Drawing.Point(390, 282);
+            this.firstFloorBtn.Location = new System.Drawing.Point(531, 363);
             this.firstFloorBtn.Name = "firstFloorBtn";
             this.firstFloorBtn.Size = new System.Drawing.Size(49, 54);
             this.firstFloorBtn.TabIndex = 3;
@@ -94,7 +107,7 @@
             // 
             this.groundFloorBtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("groundFloorBtn.BackgroundImage")));
             this.groundFloorBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.groundFloorBtn.Location = new System.Drawing.Point(390, 355);
+            this.groundFloorBtn.Location = new System.Drawing.Point(531, 448);
             this.groundFloorBtn.Name = "groundFloorBtn";
             this.groundFloorBtn.Size = new System.Drawing.Size(49, 51);
             this.groundFloorBtn.TabIndex = 4;
@@ -105,7 +118,7 @@
             // 
             this.openDoorBtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("openDoorBtn.BackgroundImage")));
             this.openDoorBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.openDoorBtn.Location = new System.Drawing.Point(351, 466);
+            this.openDoorBtn.Location = new System.Drawing.Point(495, 547);
             this.openDoorBtn.Name = "openDoorBtn";
             this.openDoorBtn.Size = new System.Drawing.Size(47, 43);
             this.openDoorBtn.TabIndex = 5;
@@ -116,7 +129,7 @@
             // 
             this.closeDoorBtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("closeDoorBtn.BackgroundImage")));
             this.closeDoorBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.closeDoorBtn.Location = new System.Drawing.Point(426, 466);
+            this.closeDoorBtn.Location = new System.Drawing.Point(566, 547);
             this.closeDoorBtn.Name = "closeDoorBtn";
             this.closeDoorBtn.Size = new System.Drawing.Size(47, 43);
             this.closeDoorBtn.TabIndex = 6;
@@ -132,11 +145,21 @@
             this.panel1.BackColor = System.Drawing.Color.Gainsboro;
             this.panel1.Controls.Add(this.logsGridView);
             this.panel1.Controls.Add(this.logsLbl);
-            this.panel1.Location = new System.Drawing.Point(547, 75);
+            this.panel1.Location = new System.Drawing.Point(719, 99);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(835, 524);
+            this.panel1.Size = new System.Drawing.Size(797, 506);
             this.panel1.TabIndex = 7;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // logsGridView
+            // 
+            this.logsGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.logsGridView.Location = new System.Drawing.Point(22, 68);
+            this.logsGridView.Name = "logsGridView";
+            this.logsGridView.RowHeadersWidth = 51;
+            this.logsGridView.RowTemplate.Height = 24;
+            this.logsGridView.Size = new System.Drawing.Size(757, 423);
+            this.logsGridView.TabIndex = 1;
             // 
             // logsLbl
             // 
@@ -149,22 +172,83 @@
             this.logsLbl.Text = "Elevator Control logs";
             this.logsLbl.Click += new System.EventHandler(this.label1_Click);
             // 
-            // logsGridView
+            // groundFloorDoorPanelL
             // 
-            this.logsGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.logsGridView.Location = new System.Drawing.Point(18, 71);
-            this.logsGridView.Name = "logsGridView";
-            this.logsGridView.RowHeadersWidth = 51;
-            this.logsGridView.RowTemplate.Height = 24;
-            this.logsGridView.Size = new System.Drawing.Size(804, 423);
-            this.logsGridView.TabIndex = 1;
+            this.groundFloorDoorPanelL.Image = ((System.Drawing.Image)(resources.GetObject("groundFloorDoorPanelL.Image")));
+            this.groundFloorDoorPanelL.Location = new System.Drawing.Point(111, 437);
+            this.groundFloorDoorPanelL.Name = "groundFloorDoorPanelL";
+            this.groundFloorDoorPanelL.Size = new System.Drawing.Size(90, 260);
+            this.groundFloorDoorPanelL.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.groundFloorDoorPanelL.TabIndex = 8;
+            this.groundFloorDoorPanelL.TabStop = false;
+            this.groundFloorDoorPanelL.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // groundFloorDoorPanelR
+            // 
+            this.groundFloorDoorPanelR.Image = ((System.Drawing.Image)(resources.GetObject("groundFloorDoorPanelR.Image")));
+            this.groundFloorDoorPanelR.Location = new System.Drawing.Point(196, 437);
+            this.groundFloorDoorPanelR.Name = "groundFloorDoorPanelR";
+            this.groundFloorDoorPanelR.Size = new System.Drawing.Size(90, 260);
+            this.groundFloorDoorPanelR.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.groundFloorDoorPanelR.TabIndex = 10;
+            this.groundFloorDoorPanelR.TabStop = false;
+            this.groundFloorDoorPanelR.Click += new System.EventHandler(this.groundFloorDoorPanelR_Click);
+            // 
+            // doorTimer
+            // 
+            this.doorTimer.Tick += new System.EventHandler(this.doorTimer_Tick);
+            // 
+            // firstFloorDoorPanelL
+            // 
+            this.firstFloorDoorPanelL.Image = ((System.Drawing.Image)(resources.GetObject("firstFloorDoorPanelL.Image")));
+            this.firstFloorDoorPanelL.Location = new System.Drawing.Point(111, 28);
+            this.firstFloorDoorPanelL.Name = "firstFloorDoorPanelL";
+            this.firstFloorDoorPanelL.Size = new System.Drawing.Size(90, 260);
+            this.firstFloorDoorPanelL.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.firstFloorDoorPanelL.TabIndex = 11;
+            this.firstFloorDoorPanelL.TabStop = false;
+            this.firstFloorDoorPanelL.Click += new System.EventHandler(this.firstFloorDoorPanelL_Click);
+            // 
+            // firstFloorDoorPanelR
+            // 
+            this.firstFloorDoorPanelR.Image = ((System.Drawing.Image)(resources.GetObject("firstFloorDoorPanelR.Image")));
+            this.firstFloorDoorPanelR.Location = new System.Drawing.Point(196, 28);
+            this.firstFloorDoorPanelR.Name = "firstFloorDoorPanelR";
+            this.firstFloorDoorPanelR.Size = new System.Drawing.Size(90, 260);
+            this.firstFloorDoorPanelR.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.firstFloorDoorPanelR.TabIndex = 12;
+            this.firstFloorDoorPanelR.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox2.Location = new System.Drawing.Point(186, 422);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(43, 20);
+            this.pictureBox2.TabIndex = 13;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox3.Location = new System.Drawing.Point(186, 12);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(43, 20);
+            this.pictureBox3.TabIndex = 14;
+            this.pictureBox3.TabStop = false;
             // 
             // Elevator_Control_System
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1394, 643);
+            this.ClientSize = new System.Drawing.Size(1554, 709);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.firstFloorDoorPanelR);
+            this.Controls.Add(this.firstFloorDoorPanelL);
+            this.Controls.Add(this.groundFloorDoorPanelR);
+            this.Controls.Add(this.groundFloorDoorPanelL);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.closeDoorBtn);
             this.Controls.Add(this.openDoorBtn);
@@ -182,6 +266,12 @@
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.logsGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.groundFloorDoorPanelL)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.groundFloorDoorPanelR)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.firstFloorDoorPanelL)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.firstFloorDoorPanelR)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -199,6 +289,13 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label logsLbl;
         private System.Windows.Forms.DataGridView logsGridView;
+        private System.Windows.Forms.PictureBox groundFloorDoorPanelL;
+        private System.Windows.Forms.PictureBox groundFloorDoorPanelR;
+        private System.Windows.Forms.Timer doorTimer;
+        private System.Windows.Forms.PictureBox firstFloorDoorPanelL;
+        private System.Windows.Forms.PictureBox firstFloorDoorPanelR;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,47 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
-using System.IO;
 using System.Windows.Forms;
 using elevator_control_system.Controllers;
 using elevator_control_system.Models;
-
 
 namespace elevator_control_system
 {
     public partial class Elevator_Control_System : Form
     {
-        int liftSpeed = 10;
+        int liftSpeed = 5;
         bool isMovingToFirstFloor = false;
         bool isMovingToGroundFloor = false;
+
+        // to check whether the elevator is in the ground floor or first floor
         bool isAtGroundFloor = true;
+        bool isAtFirstFloor = false;
 
-        int firstFloorPositionY = 20;
-        int groundFloorPositionY = 300;
+        int firstFloorPositionY = 23;
+        int groundFloorPositionY = 355;
 
-    
+        
+        private int doorSpeed = 1; // The speed at which the door moves
+
+        private bool isGroundFloorDoorOpening = false;
+        private bool isGroundFloorDoorClosing = false;
+
+
+        private bool isFirstFloorDoorOpening = false;
+        private bool isFirstFloorDoorClosing = false;
+
+
+        private int leftDoorInitialX;
+        private int rightDoorInitialX;
+        private int doorOpenMaxDistance = 60; 
+
         public Elevator_Control_System()
         {
             InitializeComponent();
             elevator.Top = groundFloorPositionY;
             fetchLatestElevatorLogs();
+
+            // Initialize door timer
+            doorTimer.Interval = 30;  // Set timer interval in milliseconds
+            doorTimer.Tick += new EventHandler(doorTimer_Tick);  // Attach tick event
+            leftDoorInitialX = groundFloorDoorPanelL.Left;
+            rightDoorInitialX = groundFloorDoorPanelR.Left;
         }
 
         // Methods to insert the elevator logs
         public void insertElevatorLogs(string action_details)
         {
-
             ElevatorLog newLog = new ElevatorLog
             {
                 Date = DateTime.Now,
                 RequestedAt = DateTime.Now.TimeOfDay,
                 Action = action_details
             };
-            ElevatorController elevatorContoller = new ElevatorController();
-
-            elevatorContoller.InsertElevatorLog(newLog);
+            ElevatorController elevatorController = new ElevatorController();
+            elevatorController.InsertElevatorLog(newLog);
         }
-
 
         // Methods to fetch the elevator logs
         private void fetchLatestElevatorLogs()
@@ -89,7 +107,6 @@ namespace elevator_control_system
             logsGridView.Refresh();
         }
 
-
         private void Form1_Load(object sender, EventArgs e)
         {
         }
@@ -127,10 +144,12 @@ namespace elevator_control_system
 
         private void openDoorBtn_Click(object sender, EventArgs e)
         {
+            openElevatorDoor();
         }
 
         private void closeDoorBtn_Click(object sender, EventArgs e)
         {
+            closeElevatorDoor();
         }
 
         private void displayBoard_Click(object sender, EventArgs e)
@@ -155,20 +174,17 @@ namespace elevator_control_system
 
         private void liftTimer_Tick(object sender, EventArgs e)
         {
-
-
-
             if (isMovingToFirstFloor && elevator.Top > firstFloorPositionY)
             {
                 disableLiftButton();
+                isAtGroundFloor = false;
+
                 displayBoard.Image = Image.FromFile(@"D:\University_Assignment\C#\elevator_control_system\Resources\up_indicator_gif.gif");
                 displayBoard.SizeMode = PictureBoxSizeMode.StretchImage;
-
 
                 firstFloorBtn.BackColor = Color.Green;
                 groundFloorBtn.BackColor = Color.White;
                 elevator.Top -= liftSpeed;
-                
 
                 if (elevator.Top <= firstFloorPositionY)
                 {
@@ -177,23 +193,22 @@ namespace elevator_control_system
 
                     elevator.Top = firstFloorPositionY;
                     firstFloorBtn.BackColor = Color.Red;
-                    isAtGroundFloor = false;
+                    isAtFirstFloor = true;
                     liftTimer.Stop();
                     groundFloorBtn.Enabled = true;
-                    
                 }
             }
             else if (isMovingToGroundFloor && elevator.Top < groundFloorPositionY)
             {
                 disableLiftButton();
+                isAtFirstFloor = false;
+
                 displayBoard.Image = Image.FromFile(@"D:\University_Assignment\C#\elevator_control_system\Resources\down_indicator.gif");
                 displayBoard.SizeMode = PictureBoxSizeMode.StretchImage;
 
                 firstFloorBtn.BackColor = Color.White;
                 groundFloorBtn.BackColor = Color.Green;
                 elevator.Top += liftSpeed;
-                
-
 
                 if (elevator.Top >= groundFloorPositionY)
                 {
@@ -205,22 +220,157 @@ namespace elevator_control_system
                     liftTimer.Stop();
                     firstFloorBtn.Enabled = true;
 
+                    isAtGroundFloor = true;
+                    
                 }
             }
         }
 
+        // Timer tick event to animate the door
+        private void doorTimer_Tick(object sender, EventArgs e)
+        {
+            if (isAtGroundFloor)
+            {
+                if (isGroundFloorDoorOpening)
+                {
+                    // Move the left door to the left
+                    if (groundFloorDoorPanelL.Left > leftDoorInitialX - doorOpenMaxDistance)
+                    {
+                        groundFloorDoorPanelL.Left -= doorSpeed;
+                    }
+
+                    // Move the right door to the right
+                    if (groundFloorDoorPanelR.Left < rightDoorInitialX + doorOpenMaxDistance)
+                    {
+                        groundFloorDoorPanelR.Left += doorSpeed;
+                    }
+
+                    // Stop the timer once the door is fully opened
+                    if (groundFloorDoorPanelL.Left <= leftDoorInitialX - doorOpenMaxDistance &&
+                        groundFloorDoorPanelR.Left >= rightDoorInitialX + doorOpenMaxDistance)
+                    {
+                        doorTimer.Stop();
+                    }
+                }
+                else if (isGroundFloorDoorClosing)
+                {
+                    if (groundFloorDoorPanelL.Left < leftDoorInitialX)
+                    {
+                        groundFloorDoorPanelL.Left += doorSpeed;
+                    }
+
+                    if (groundFloorDoorPanelR.Left > rightDoorInitialX)
+                    {
+                        groundFloorDoorPanelR.Left -= doorSpeed;
+                    }
+
+                    if (groundFloorDoorPanelL.Left >= leftDoorInitialX &&
+                        groundFloorDoorPanelR.Left <= rightDoorInitialX)
+                    {
+                        doorTimer.Stop();
+                    }
+                }
+            }
+            
+            else if (isAtFirstFloor)
+            {
+                if (isFirstFloorDoorOpening)
+{
+    // Move the left door to the left
+    if (firstFloorDoorPanelL.Left > leftDoorInitialX - doorOpenMaxDistance)
+    {
+        firstFloorDoorPanelL.Left -= doorSpeed;
+    }
+
+    // Move the right door to the right
+    if (firstFloorDoorPanelR.Left < rightDoorInitialX + doorOpenMaxDistance)
+    {
+        firstFloorDoorPanelR.Left += doorSpeed;
+    }
+
+    // Stop the timer once the door is fully opened
+    if (firstFloorDoorPanelL.Left <= leftDoorInitialX - doorOpenMaxDistance &&
+        firstFloorDoorPanelR.Left >= rightDoorInitialX + doorOpenMaxDistance)
+    {
+        doorTimer.Stop();
+    }
+}
+else if (isFirstFloorDoorClosing)
+{
+    if (firstFloorDoorPanelL.Left < leftDoorInitialX)
+    {
+        firstFloorDoorPanelL.Left += doorSpeed;
+    }
+
+    if (firstFloorDoorPanelR.Left > rightDoorInitialX)
+    {
+        firstFloorDoorPanelR.Left -= doorSpeed;
+    }
+
+    if (firstFloorDoorPanelL.Left >= leftDoorInitialX &&
+        firstFloorDoorPanelR.Left <= rightDoorInitialX)
+    {
+        doorTimer.Stop();
+    }
+}
+
+            }
+        }
+
+        private void openElevatorDoor()
+        {
+            if (isAtGroundFloor)
+            {
+                isGroundFloorDoorOpening = true;
+                isGroundFloorDoorClosing = false;
+                doorTimer.Start();
+            }
+            else if (isAtFirstFloor)
+            {
+                isFirstFloorDoorOpening = true;
+                isFirstFloorDoorClosing = false;
+                doorTimer.Start(); 
+            }
+        }
+
+        private void closeElevatorDoor()
+        {
+            if (isAtGroundFloor)
+            {
+                isGroundFloorDoorOpening = false;
+                isGroundFloorDoorClosing = true;
+                doorTimer.Start();
+            }
+            else if (isAtFirstFloor)
+            {
+                isFirstFloorDoorOpening = false;
+                isFirstFloorDoorClosing = true; 
+                doorTimer.Start();
+            }
+        }
 
         private void panel1_Paint(object sender, PaintEventArgs e)
         {
-
         }
 
         private void label1_Click(object sender, EventArgs e)
         {
-
         }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void firstFloorDoorPanelL_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groundFloorDoorPanelR_Click(object sender, EventArgs e)
         {
 
         }
